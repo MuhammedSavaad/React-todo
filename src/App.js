d2d692b4f33a0e8b3b4ce8f5aac059683cdb7bee
savadMKD <savadmoorkanad@gmail.com>
2022-04-07 16:53:58 +0530

    Header Designed!..

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Todo App</h1>
-    </div>
+    <main>
+      <Header />
+    </main>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header>
+        <h1>Hey 👋</h1>
+        <p>Welcome To TO-DO App 😉💗</p>
+    </header>
+  )
+}
+
+export default Header;
