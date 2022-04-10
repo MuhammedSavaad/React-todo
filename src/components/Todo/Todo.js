c31a764689f779ed06ab9bf1eef8786734bd8027
savadMKD <savadmoorkanad@gmail.com>
2022-04-10 12:07:51 +0530

    Delete Function Completed

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -16,7 +16,10 @@ function Todo() {
              placeholder="Enter To-Do..."
             />
 
-            <img onClick={()=>setToDos([...toDos, toDo])} src="/images/add.png" alt="add-button" />
+            <img onClick={()=>{
+              setToDos([...toDos, {id:Date.now(), text:toDo, status:false}]);
+              setToDo('');
+            }} src="/images/add.png" alt="add-button" />
         </div>
 
         <div className="todo-bar">
@@ -24,12 +27,43 @@ function Todo() {
               toDos.map((value, index)=>{
                 return(
                   <div className="todo" key={index}>
-                    <p>{value}</p>
-                    <img src="/images/trash.png" alt="trash-button" />
+                    <p>{value.text}</p>
+                    <label className="container">
+                      <input onChange={(e)=>{
+                        console.log(e.target.checked);
+                        console.log(value);
+                        setToDos(toDos.filter(obj=>{
+                          if(obj.id===value.id){
+                            obj.status=e.target.checked;
+                          }
+                          return obj;
+                        }))
+                      }} value={value.status} type="checkbox" />
+                      <span className="checkmark"></span>
+                    </label>
+                    <img onClick={() =>{
+                      setToDos(toDos.filter(obj => {
+                        let temp;
+                        if (obj.id != value.id){
+                          temp = obj
+                        }
+                        return temp;
+                      }));
+                    }} src="/images/trash.png" alt="trash-button" />
                   </div>
                 )
               })
             }
+
+            {/* {toDos.map((value) => {
+              if(value.status){
+                return(
+                  <h1>{value.text}</h1>
+                )
+              }
+
+              return null;
+            })} */}
         </div>
     </section>
   )
